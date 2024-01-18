4ea9fa2fedc9b3f4e5094dae96bba7055b3f007b
dthabata <dthabatita@gmail.com>
2024-01-18 15:55:57 -0300

    feat: first simple Home at controllers

--- src/app.js
+++ src/app.js
@@ -1,7 +1,11 @@
 require('dotenv').config();
 const express = require('express');
 
+const Home = require('./controllers/Home');
+
 const app = express();
+
+console.log(Home.index());
 
 app.listen(process.env.PORT || 3000, () => {
     console.log("Servidor inicializado")
---
+++ src/controllers/Home.js
@@ -0,0 +1,5 @@
+const index = () => {
+    return 'Home';
+}
+
+module.exports = { index };
