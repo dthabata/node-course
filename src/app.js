a113f4f9500fd5274a795ae4d75590865b7fbd77
dthabata <dthabatita@gmail.com>
2024-01-23 17:58:54 -0300

    feat: testing middlewares

--- src/app.js
+++ src/app.js
@@ -5,8 +5,18 @@ const Login = require('./controllers/Login');
 
 const app = express();
 
+// app.use('/signup', function (request, response, next) {
+//     return response.json("middleware");
+//     request.name = "Thabata";
+//     request.age = 30;
+//     next();
+//     return response.json('signup');
+// });
+
 app.get("/", Home.index);
-app.post("/login", Login.store);
+app.post("/login", function (request, response, next) {
+    return response.json("middleware login")
+}, Login.store);
 
 app.listen(process.env.PORT || 3000, () => {
     console.log("Servidor inicializado")
--- src/controllers/Home.js
+++ src/controllers/Home.js
@@ -1,5 +1,5 @@
 const index = function (request, response) {
-    return response.json("home");
+    return response.json({ name: request.name, age: request.age });
 };
 
 module.exports = { index };
