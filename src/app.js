72e4237073f68682eee18933ad6a94b4678fb68d
dthabata <dthabatita@gmail.com>
2024-01-24 13:58:12 -0300

    feat: removes test post login

--- src/app.js
+++ src/app.js
@@ -1,14 +1,10 @@
 require('dotenv').config();
 const express = require('express');
 const Home = require('./controllers/Home');
-const Login = require('./controllers/Login');
 
 const app = express();
 
 app.get("/", Home.index);
-app.post("/login", function (request, response, next) {
-    return response.json("middleware login")
-}, Login.store);
 
 app.listen(process.env.PORT || 3000, () => {
     console.log("Servidor inicializado")
