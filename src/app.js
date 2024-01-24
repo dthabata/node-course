1e1063d0ae6f2cfda52a632c6a14c67a6a4b1e83
dthabata <dthabatita@gmail.com>
2024-01-24 14:36:16 -0300

    fix: renaming config in app

--- src/app.js
+++ src/app.js
@@ -3,12 +3,16 @@ const express = require('express');
 const app = express();
 var exphbs = require('express-handlebars');
 
-app.engine('handlebars', exphbs());
-app.set('view engine', 'handlebars');
+app.engine('html', exphbs({
+    partialsDir: '/partials',
+    layoutsDir: '/layouts',
+}));
+
+app.set('view engine', 'html');
+app.set('views', '/views');
 
 app.use('/', require('./routes/site'));
 app.use('/post', require('./routes/post'));
-
 
 app.listen(process.env.PORT || 3000, () => {
     console.log("Servidor inicializado")
